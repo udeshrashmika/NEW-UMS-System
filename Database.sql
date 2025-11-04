ca1a208253eb06cb625dab36cd5385943ad4925e
Udesh Rashmika <apurdesilva@students.nsbm.ac.lk>
2025-11-04 08:38:17 +0530

    Update Database.sql

--- Database.sql
+++ Database.sql
@@ -1,6 +1,19 @@
 CREATE DATABASE UMS_DATABASE;
 USE UMS_DATABASE;
--- 1. Create Database
+
+CREATE TABLE Customer (
+    CustomerID NVARCHAR(20) PRIMARY KEY NOT NULL,
+    CustomerName NVARCHAR(100) NOT NULL,
+    CustomerType NVARCHAR(20) NOT NULL,
+    ServiceAddress NVARCHAR(255) NOT NULL,
+    BillingAddress NVARCHAR(255) NULL,
+    Email NVARCHAR(100) UNIQUE NULL,
+    Phone NVARCHAR(20) NULL,
+    RegistrationDate DATE NOT NULL,
+    -- Add CHECK constraint for ENUM values
+    CONSTRAINT CHK_CustomerType CHECK (CustomerType IN ('Household', 'Business', 'Government'))
+);
+GO
 
 
 
